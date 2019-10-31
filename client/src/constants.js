649c13e5d0379b1c34c1dc6f80c3a219468721f2
nichanank <n.kesonpat@gmail.com>
2019-11-01 05:06:37 +0700

    update index.js and submission constants

---
+++ client/src/constants.js
@@ -0,0 +1,17 @@
+export const actions = [
+  'Conserve energy – remember to turn off lights, computers, and electric appliances when not in use',
+  'Use energy efficient light bulbs and appliances',
+  'Participate in your local utility’s energy conservation programs',
+  'Limit driving by carpooling, using public transportation, biking and walking',
+  'Combine errands for fewer trips',
+  'Keep your automobile well tuned and maintained.',
+  'Avoid excessive idling of your automobile',
+  'Drive electric or plug-in hybrid electric vehicles',
+  'Use electric or hand-powered lawn care equipment',
+  'Mulch or compost leaves and yard waste',
+  'Be careful not to spill gasoline when filling up your car or gasoline powered lawn and garden equipment',
+  'Run dishwashers and clothes washers only when full',
+  'Choose environmentally friendly cleaners',
+  'Seal containers of household cleaners, workshop chemicals and solvents, and garden chemicals to prevent volatile organic compounds from evaporating into the air',
+  'Advocate for emission reductions from power plants and more stringent national vehicle emission standards'
+]
--- client/src/index.js
+++ client/src/index.js
@@ -2,22 +2,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
-
-// const Network = new NetworkOnlyConnector({ providerURL: process.env.REACT_APP_NETWORK_URL || '' })
-// const Injected = new InjectedConnector({ supportedNetworks: [Number(process.env.REACT_APP_NETWORK_ID || '1')] })
-// const connectors = { Injected, Network }
-// console.log(process.env.REACT_APP_NETWORK_ID)
 import { connectors } from './connectors'
-
 import Web3Provider from 'web3-react'
 import ThemeProvider, { GlobalStyle } from './theme'
 
 import App from './pages/App'
-// import './i18n'
 
 import LocalStorageContextProvider, { Updater as LocalStorageContextUpdater } from './contexts/LocalStorage'
 import ApplicationContextProvider, { Updater as ApplicationContextUpdater } from './contexts/Application'
-import TransactionContextProvider, { Updater as TransactionContextUpdater } from './contexts/Transactions'
 
 require('./pages/App.css')
 require('materialize-css/dist/css/materialize.min.css')
@@ -27,9 +19,7 @@ function ContextProviders({ children }) {
   return (
     <LocalStorageContextProvider>
       <ApplicationContextProvider>
-        <TransactionContextProvider>
             {children}
-        </TransactionContextProvider>
       </ApplicationContextProvider>
     </LocalStorageContextProvider>
   )
@@ -40,7 +30,6 @@ function Updaters() {
     <>
       <LocalStorageContextUpdater />
       <ApplicationContextUpdater />
-      <TransactionContextUpdater />
     </>
   )
 }
