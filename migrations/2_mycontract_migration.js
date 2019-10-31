12019e60dfcb188f95705ae80615504f8f717e8d
nichanank <n.kesonpat@gmail.com>
2019-11-01 04:40:29 +0700

    update migrations file to include continuousToken contract deployment

--- migrations/2_mycontract_migration.js
+++ migrations/2_mycontract_migration.js
@@ -1,7 +1,11 @@
 let MyContract = artifacts.require('MyContract')
 let LinkToken = artifacts.require('LinkToken')
+let ContinuousToken = artifacts.require('ContinuousToken')
 let Oracle = artifacts.require('Oracle')
 
+const DECIMALS = 2;
+const INITIAL_SUPPLY = web3.utils.toWei('1', 'ether');
+const RESERVE_RATIO = 100000;
 
 module.exports = (deployer, network) => {
   // Local (development) networks need their own deployment of the LINK
@@ -9,12 +13,16 @@ module.exports = (deployer, network) => {
   if (!network.startsWith('live')) {
     deployer.deploy(LinkToken).then(() => {
       return deployer.deploy(Oracle, LinkToken.address).then(() => {
-        return deployer.deploy(MyContract, LinkToken.address)
+        return deployer.deploy(ContinuousToken, 'Air Token', 'AIR', DECIMALS, INITIAL_SUPPLY, RESERVE_RATIO).then(() => {
+          return deployer.deploy(MyContract, LinkToken.address, ContinuousToken.address)
+        })
       })
     })
   } else {
     // For live networks, use the 0 address to allow the ChainlinkRegistry
     // contract automatically retrieve the correct address for you
-    deployer.deploy(MyContract, '0x0000000000000000000000000000000000000000')
+    deployer.deploy(ContinuousToken, 'Air Token', 'AIR', DECIMALS, INITIAL_SUPPLY, RESERVE_RATIO).then(() => {
+      return deployer.deploy(MyContract, '0x0000000000000000000000000000000000000000', ContinuousToken.address)
+    })
   }
 }
