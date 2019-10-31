4aa5198b25bf95fe099b39017dda5c2f5f865fab
nanspro <rahulbishnoi3636@gmail.com>
2019-10-31 18:49:03 +0530

    Adding claim and review

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -2,6 +2,7 @@ pragma solidity 0.4.24;
 
 import "chainlink/contracts/ChainlinkClient.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./reserve/ContinuousToken.sol";
 
 /**
  * @title MyContract is an example contract which requests data from
@@ -11,14 +12,43 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
  */
 contract MyContract is ChainlinkClient, Ownable {
   uint256 public data;
+  mapping(address => uint256) public scores;
+  address[] users;
+  enum Status { pending, accepted, rejected }
+
+  uint256 totalClaims;
+  uint256 totalScore;
+  bool reedemPhase;
+  struct Multihash {
+    bytes32 digest;
+    uint8 hashFunction;
+    uint8 size;
+  }
+
+  struct Submission {
+    uint256 submissionId;
+    address user;
+    Multihash ipfsHash;
+    Status status;
+  }
+
+  Submission[] submissions;
+
   event Data(uint256 data, bytes32 requestId);
+  event ClaimSubmitted(uint256 indexed submissionId, address indexed user, Mutlihash indexed ipfsHash, Status st);
+  event ClaimReviewed(uint256 indexed submissionId, address indexed user, Mutlihash indexed ipfsHash, Status st);
+  event RedeemPhase(bool on);
+   
   /**
    * @notice Deploy the contract with a specified address for the LINK
    * and Oracle contract addresses
    * @dev Sets the storage for the specified addresses
    * @param _link The address of the LINK token contract
    */
-  constructor(address _link) public {
+  constructor(address _link, address _token) public {
+    totalClaims = 0;
+    totalScore = 0;
+    reedemPhase = false;
     if (_link == address(0)) {
       setPublicChainlinkToken();
     } else {
@@ -34,6 +64,57 @@ contract MyContract is ChainlinkClient, Ownable {
   function getChainlinkToken() public view returns (address) {
     return chainlinkTokenAddress();
   }
+
+  // Submit a claim that you helped with the cause and earn score.
+  function claim(bytes32 digest, uint8 hashFunction, uint8 size) public returns (uint256) {
+    Multihash storage ipfsHash = Multihash(digest, hashFunction, size);
+    submissions[totalClaims] = Submission(totalClaims, msg.sender, ipfsHash, pending);
+    totalClaims = totalClaims + 1;
+    // scores[msg.sender] = scores[msg.sender] + 1;
+    emit ClaimSubmitted(totalClaims - 1, msg.sender, ipfsHash, pending);
+    return totalClaims;
+  }
+
+  function reviewSubmission(uint256 submissionId, bool accepted) public onlyOwner {
+    require(submissions[submissionId].status == pending, "Claim has already been taken care of");
+    if (accepted) {
+      submissions[submissionId].status = accepted;
+    }
+    else {
+      submissions[submissionId].status = rejected;
+    }
+    scores[submissions[submissionId].user] = scores[submissions[submissionId].user] + 1;
+    totalScore = totalScore + 1;
+    emit ClaimReviewed(submissionId, msg.sender, ipfsHash, submissions[submissionId].status);
+  }
+
+  function targetAchieved() internal {
+    // transfer tokens to all users
+    reedemPhase = true;
+    emit RedeemPhase(true);
+  }
+
+  function redeem() public {
+    require(redeemPhase == true, "You cannot redeem now");
+    if (scores[msg.sender] != 0){
+      uint256 amount = scores[msg.sender] * totalSupply();
+      amount = amount / totalScore;
+      // transfer that much tokens
+      scores[msg.sender] = 0;
+    }
+  }
+  // View your score
+  function getScore() public view returns (uint256) {
+    return scores[msg.sender];
+  }
+
+  function getHash(uint submissionId) public view returns (bytes32 digest, uint8 hashFunction, uint8 size) {
+    Multihash storage data = submissions[submissionId].ipfsHash;
+    return (data.digest, data.hashFunction, data.size);
+  }
+
+
+
 
   /**
    * @notice Creates a request to the specified Oracle contract address
@@ -76,8 +157,10 @@ contract MyContract is ChainlinkClient, Ownable {
     recordChainlinkFulfillment(_requestId)
   {
     data = _data;
+    if (data > 20){
+      targetAchieved();
+    }
     emit Data(data, _requestId);
-    
   }
 
   /**
--- scripts/request-data.js
+++ scripts/request-data.js
@@ -1,5 +1,5 @@
 const MyContract = artifacts.require('MyContract')
-const ContinuousToken = artifacts.require('ContinuousToken')
+// const ContinuousToken = artifacts.require('ContinuousToken')
 /*
   This script allows for a Chainlink request to be created from
   the requesting contract. Defaults to the Chainlink oracle address
@@ -10,7 +10,7 @@ const oracleAddress =
   process.env.TRUFFLE_CL_BOX_ORACLE_ADDRESS ||
   '0x14301ce4eb78b591a5437bf4d6e592ba3ab1d742'
 const jobId =
-  process.env.TRUFFLE_CL_BOX_JOB_ID || '5a1667305a184dc98ccc5db3d6df34d0'
+  process.env.TRUFFLE_CL_BOX_JOB_ID || '6a2662fb442142e997257e7fc7fa1fe9'
 const payment = process.env.TRUFFLE_CL_BOX_PAYMENT || '1000000000000000000'
 const url =
   process.env.TRUFFLE_CL_BOX_URL ||
@@ -20,8 +20,10 @@ const times = process.env.TRUFFLE_CL_BOX_TIMES || '2'
 
 module.exports = async callback => {
   const mc = await MyContract.deployed()
-  const res = await ContinuousToken.deployed()
+//  const res = await ContinuousToken.deployed()
   console.log('Creating request on contract:', mc.address)
+  // const tx2 = await res.mint.call({from: '0xAF06950aa322877B640d9533163a19C739DA1a1D', value:100000000000000000})
+
   const tx = await mc.createRequestTo(
     oracleAddress,
     web3.utils.toHex(jobId),
@@ -31,6 +33,5 @@ module.exports = async callback => {
     times,
   )
 
-  const tx2 = await res.mint.call({from: '0xAF06950aa322877B640d9533163a19C739DA1a1D', value:100000000000000000})
   callback(tx.tx)
 }
